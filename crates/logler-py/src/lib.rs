8ec538f40ce85de491ac7602636f9b65aab2e9f7
Gabu <gabu@gabu.quest>
2026-01-16 11:16:30 +0900

    Remove SQL feature from logler-py bindings
    - Remove sql feature from logler-py Cargo.toml
    - Remove sql_query, sql_tables, sql_schema PyO3 bindings
    
    SQL functionality is now provided by Python module directly.

--- crates/logler-py/src/lib.rs
+++ crates/logler-py/src/lib.rs
@@ -135,30 +135,6 @@ impl PyInvestigator {
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
     }
 
-    /// Execute SQL query on loaded logs (requires 'sql' feature)
-    #[cfg(feature = "sql")]
-    fn sql_query(&self, query: String) -> PyResult<String> {
-        self.investigator
-            .sql_query(&query)
-            .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
-    }
-
-    /// Get available SQL tables (requires 'sql' feature)
-    #[cfg(feature = "sql")]
-    fn sql_tables(&self) -> PyResult<Vec<String>> {
-        self.investigator
-            .sql_tables()
-            .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
-    }
-
-    /// Get SQL table schema (requires 'sql' feature)
-    #[cfg(feature = "sql")]
-    fn sql_schema(&self, table: String) -> PyResult<String> {
-        self.investigator
-            .sql_schema(&table)
-            .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
-    }
-
     /// Build hierarchical view of threads/spans
     fn build_hierarchy(
         &self,
